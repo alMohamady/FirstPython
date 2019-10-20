1e3415faf8d5a6b8b348d567306a45d28d310876
al Mohamady <al.Mohamady47@gmail.com>
2019-10-20 20:57:07 +0200

    USER INPUT IN PYTHON

--- MyCode.py
+++ MyCode.py
@@ -1,5 +1,23 @@
+import sys
+
 print("Hello World")
-a = 7
-b = 5
+print("My First Code")
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = a + b
+print(c)
+
+print("Hello World")
+
+result = eval(input("Enter the operation"))
+print(result)
+
+
+ch = input("Enter the Char")[0]
+print(ch)
+
+
+a = int(input("Enter the a number: "))
+b = int(input("Enter the b number: "))
 c = a + b
 print(c)
