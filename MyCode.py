aa2972a67709401f0f8ba0fcbe186b6af3878a88
al Mohamady <al.Mohamady47@gmail.com>
2019-10-22 14:54:33 +0200

    BREAK, CONTINUE AND PASS

--- MyCode.py
+++ MyCode.py
@@ -1,13 +1,36 @@
 
-for x in range(23, 10, -2):
-    if x % 5 != 0:
-         print(x)
+for i in range(1, 200):
 
-for r in [3, 2.8, "Mohamed"]:
-    print(r)
+    if i % 5 == 0:
+       pass
+    else:
+       print(i)
 
-x = "al-Mohamady"
+print("Hello world")
+#===================================================
 
-for i in x:
-     print(i)
+for i in range(1, 200):
 
+    if i % 5 != 0:
+        continue
+
+    print(i)
+
+print("Hello world")
+
+#===================================================
+
+x = int(input("How Many Boxes in the inventory: "))
+a = 20
+
+i = 1
+while i <= x:
+
+    if i > a:
+        print("Error: Out of Stock")
+        break
+
+    print("Box Num " + str(i))
+    i += 1
+
+print("Hello World")
