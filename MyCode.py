c6728d91b5900fbbf5366d5d9b20b536ada96906
al Mohamady <al.Mohamady47@gmail.com>
2019-10-22 13:49:57 +0200

    WHILE LOOP

--- MyCode.py
+++ MyCode.py
@@ -1,29 +1,13 @@
 
 
-x = 5
-
-if x == 1:
-    print("x = 1")
-elif x == 2:
-    print("x = 2")
-elif x == 3:
-    print("x = 3")
-else:
-    print("Nothing")
+i = 5
 
 
-a = 2
-b = a % 2
-
-if b == 0:
-    print("Even")
-    if a > 10:
-        print("big number")
-    else:
-        print("Small")
-else:
-    print("odd")
-
-
-print("it's Oky")
-
+while i >= 1:
+    print("al Mohamady -> ", end="")
+    j = 1
+    while j <= 5:
+        print("*", end="")
+        j += 1
+    i = i - 1
+    print()
