0ea2ad6121f28cd0179a3e6c4c3d6bdf7b4e8d9d
al Mohamady <al.Mohamady47@gmail.com>
2019-10-23 02:33:12 +0200

    ARRAY IN PYTHON

--- MyCode.py
+++ MyCode.py
@@ -1,9 +1,18 @@
+from array import *
 
-numbers = [11, 22, 33, 40, 55, 115, 20]
+arr = array('i', [5, 6, 7, 8])
+arr2 = array('u', ['a', 'b', 'c', 'd'])
 
-for number in numbers:
-    if number % 2 == 0 and number % 3 == 0:
-        print(number)
-        break
-else:
-    print("Not found")
+arr3 = array(arr.typecode, (a + 10 for a in arr))
+
+for i in arr3:
+    print(i)
+
+for i in arr2:
+    print(i)
+
+for i in range(len(arr)):
+    print(arr[i])
+
+for i in arr:
+    print(i)
