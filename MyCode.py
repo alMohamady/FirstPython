541a7883b66b3f1625b51ee84a471779496b6654
al Mohamady <al.Mohamady47@gmail.com>
2019-10-27 23:03:03 +0200

    INSTALLING NUMPY

--- MyCode.py
+++ MyCode.py
@@ -1,23 +1,5 @@
-from array import *
+from numpy import *
 
-arr = array('i', [])
-
-num = int(input("Enter the array count: "))
-
-for i in range(num):
-    x = int(input("Enter the value number " + str(i) + " : "))
-    arr.append(x)
+arr = array([1,2,3,4,5,6,7], int)
 
 print(arr)
-
-val = int(input("Enter the value : "))
-
-index = 0
-for i in arr:
-    if i == val:
-        print(str(i) + " With index  " + str(index))
-        break
-    index += 1
-
-
-print(str(val) + " With index  " + str(arr.index(val)))
