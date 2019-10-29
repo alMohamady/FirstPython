1d9e12d3696f77630b411a4756adbe71753e35f7
al Mohamady <al.Mohamady47@gmail.com>
2019-10-29 12:42:57 +0200

    WAYS OF CREATING ARRAYS IN NUMPY

--- MyCode.py
+++ MyCode.py
@@ -1,5 +1,22 @@
 from numpy import *
 
-arr = array([1,2,3,4,5,6,7], int)
+arr6 = ones(10, int)
+print(arr6)
 
+arr5 = zeros(5)
+print(arr5)
+
+arr4 = logspace(1, 15, 20)
+print(arr4)
+
+arr3 = arange(1, 15, 2)
+print(arr3)
+
+arr2 = linspace(0, 15)
+print(arr2)
+
+arr = array([1, 2, 3, 4.5, 5, 6, 7])
+print(arr.dtype)
 print(arr)
+
+
