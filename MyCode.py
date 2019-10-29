d8560095ffb91e9027787b312c62b930c723e89b
al Mohamady <al.Mohamady47@gmail.com>
2019-10-29 13:59:53 +0200

    WORKING WITH MATRIX

--- MyCode.py
+++ MyCode.py
@@ -1,15 +1,25 @@
 from numpy import *
 
-arr0 = array([11, 22, 33, 44, 55])
-arrZ = arr0.copy()
+mat = array([
+            [1, 2, 3, 7, 8, 9],
+            [4, 5, 6, 11, 12, 13]
+            ])
 
-arr0[1] = 88
+m1 = matrix("1 2 3 ; 4 5 6 ; 7 8 9")
+m2 = matrix("1 2 3 ; 4 5 6 ; 7 8 9")
 
-print(arr0)
-print(arrZ)
+m3 = m1 + m2
+print(m3)
+print(m1.min())
+print(diagonal(m1))
+mat2 = mat.flatten()
+mat3 = mat.reshape(2, 2, 3)
 
-print(id(arr0))
-print(id(arrZ))
+print(mat3)
 
-
+print(mat)
+print(mat2)
+print(mat.shape)
+print(mat.size)
+print(mat.ndim)
 
