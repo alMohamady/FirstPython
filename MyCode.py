be38b5c0372f883c41913fe8923bfb1817581f5b
al Mohamady <al.Mohamady47@gmail.com>
2019-10-29 22:43:06 +0200

    PASS LIST

--- MyCode.py
+++ MyCode.py
@@ -1,15 +1,19 @@
 
-a = 20
-print(id(a))
 
-def func():
-    a = 30
-    print(id(a))
-    x = globals()['a']
-    print(id(x))
-    print(" FUNC A :", a)
-    print(x)
+def countList(lst):
+    even = 0
+    odd = 0
+
+    for i in lst:
+        if i % 2 == 0:
+            even += i
+        else:
+            odd += i
+    return even, odd
 
 
-func()
-print("GLOBAL A:", a)
+lst = [1, 2, 3, 4, 5, 6, 7]
+even, odd = countList(lst)
+
+print(even)
+print(odd)
