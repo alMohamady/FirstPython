5498725b046eaaa23184c0fde77f13fc06786a02
al Mohamady <al.Mohamady47@gmail.com>
2019-11-07 01:31:03 +0200

    Types of Variables

--- MyCode.py
+++ MyCode.py
@@ -2,25 +2,19 @@
 
 class Car:
 
+    seats = 4
+
     def __init__(self):
         self.name = "golf"
         self.speed = 180
 
-    def update(self):
-        self.name = "passat"
-
-    def compare(self, other):
-        if self.name == other.name:
-            return True
-        else:
-            return False
 
 polo = Car()
 mini = Car()
 
-print(polo.name)
-print(mini.name)
+Car.seats = 2
 
-if polo.compare(mini):
-    print("they are same")
+print(polo.seats)
+print(mini.seats)
+
 
