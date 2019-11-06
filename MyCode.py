eaae2ad1ffb389ca70de9b6a4771bd7c7f58c888
al Mohamady <al.Mohamady47@gmail.com>
2019-11-07 00:01:32 +0200

    CONSTRUCTOR __init__ METHOD

--- MyCode.py
+++ MyCode.py
@@ -2,22 +2,18 @@
 
 class Car:
 
-    x = 1
+    def __init__(self, name, speed):
+        print("Hello there ", name, speed)
+        self.name = name
+        self.speed = speed
 
     def drive(self):
-        print("Driving the car")
+        print("Driving the car", self.name, self.speed)
 
 
 a = 8
-polo = Car()
-mini = Car()
-
-#Car.drive(polo)
-#Car.drive(mini)
+polo = Car("polo", 180)
+mini = Car("mini", 200)
 
 polo.drive()
 mini.drive()
-
-print(type(a))
-a.bit_length()
-print(type(polo))
